4a971fae4acfdfec2ff40f1b2b5218e3f8130832
Vusal Kazimli <kazimlivusal8@gmail.com>
2023-07-25 15:47:46 +0400

    versiya 2.0

--- main.js
+++ main.js
@@ -5,8 +5,40 @@ const leftNavClose = document.querySelector(".logo");
 const QeydiyyaT = document.querySelector(".qeydiyyat");
 const formCloseBtn = document.querySelector(".form-close-btn");
 const formOpenBtn = document.querySelector(".btn_signIn");
+const BtnWatchlist = document.querySelector(".btn_watchlist");
+const WatchList = document.querySelector(".watchlist_menu");
+const BtnWatchlistClose = document.querySelector(".watchlist_close_menu");
+const Delete = document.querySelector(".delete");
+const Delete2 = document.querySelector(".delete2");
+const Delete4 = document.querySelector(".delete4");
+const Delete3 = document.querySelector(".delete3");
+const Movie = document.querySelector(".movie");
+const Movie2 = document.querySelector(".movie2");
+const Movie3 = document.querySelector(".movie3");
+const Movie4 = document.querySelector(".movie4");
+const MovieClose = document.querySelector(".movie--close");
 
 
+
+
+Delete.addEventListener("click",function(){
+  Movie.classList.add("movie--close");
+})
+Delete2.addEventListener("click",function(){
+  Movie2.classList.add("movie--close2");
+})
+Delete3.addEventListener("click",function(){
+  Movie3.classList.add("movie--close3");
+})
+Delete4.addEventListener("click",function(){
+  Movie4.classList.add("movie--close4");
+})
+BtnWatchlist.addEventListener("click",function(){
+  WatchList.classList.add('watchlist_menu-open');
+})
+BtnWatchlistClose.addEventListener("click",function(){
+  WatchList.classList.remove('watchlist_menu-open');
+})
 navBars.addEventListener("click",function(){
   navResponse.classList.add('nav-responsive-open');
   leftNavClose.classList.add("left_nav-close");
